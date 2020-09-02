b4aadd53bbb639ce4ad1c4cc9bae45f88a0585c3
steve.cai <steve.cai@morningstar.com>
2020-09-02 19:47:04 +0800

    Commit

--- App/src/components/AppDevInit.vue
+++ App/src/components/AppDevInit.vue
@@ -4,28 +4,81 @@
       <div class="flex-1"></div>
       <div class="container bd">
         <el-steps :active="step" simple>
-          <el-step title="初始化须知" icon="el-icon-edit"></el-step>
-          <el-step title="选择继承项目" icon="el-icon-edit"></el-step>
+          <el-step title="须知" icon="el-icon-edit"></el-step>
+          <el-step title="起始项目" icon="el-icon-edit"></el-step>
+          <el-step title="选择组件" icon="el-icon-edit"></el-step>
           <el-step title="初始化" icon="el-icon-s-grid"></el-step>
           <el-step title="完成初始化" icon="el-icon-success"></el-step>
         </el-steps>
-        <div class="padding ">
-          <div  v-if="step==1">
+        <div class="padding">
+          <div v-if="step==1">
             <div v-html="initprivacy" class="step1content bd padding"></div>
             <div class="margin-top">
-                <el-checkbox v-model="checked">知道了</el-checkbox>
+              <el-checkbox v-model="checked">知道了</el-checkbox>
             </div>
-              <div class="margin-top text-center">
-                <el-button type="primary" round :disabled="checked==false" @click="step=2">下一步</el-button>
-              </div>
+            <div class="margin-top text-center">
+              <el-button type="primary" round :disabled="checked==false" @click="step=2">下一步</el-button>
+            </div>
           </div>
           <div v-if="step==2">
-              <div class="h3">系统模版项目</div>
+            <div class="h3">系统模板列表</div>
+            <div class="margin-top">
+              <el-table :data="sysproductlist" style="width: 100%">
+                <el-table-column prop="name" label="名称" width="180"></el-table-column>
+                <el-table-column prop="summary" label="描述"></el-table-column>
+                <el-table-column width="180" label="操作">
+                  <template slot-scope="scope">
+                    <el-button size="mini" @click="viewdetail(scope.row.id)">查看</el-button>
+                    <el-button size="mini" type="primary" @click="select(scope.row.id)">选择</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
+          </div>
+          <div v-if="step==3">
+            <div class="h3">系统插件列表</div>
+            <div class="margin-top">
+              <el-table :data="pluginlist" style="width: 100%">
+                <el-table-column
+                  type="selection"
+                  width="55">
+                </el-table-column>
+                <el-table-column prop="name" label="名称" width="180"></el-table-column>
+                <el-table-column prop="summary" label="描述"></el-table-column>
+              </el-table>
+            </div>
           </div>
         </div>
       </div>
       <div class="flex-1"></div>
     </div>
+    <el-dialog
+      v-if="productinfo!=null"
+      title="项目详情"
+      :visible.sync="showproductinfo"
+      center
+      width="800"
+    >
+      <div class="flex-row">
+        <div class="flex-1 margin-right">
+          <div class="h3">{{productinfo.name}}</div>
+        </div>
+        <div>
+          <el-button type="primary" @click="select(productinfo.id)">选择使用</el-button>
+        </div>
+      </div>
+      <div class="f-g margin-top">{{productinfo.summary}}</div>
+      <div class="margin-top">
+        <el-tabs v-model="activeName">
+          <el-tab-pane label="项目介绍" name="first">
+            <div class="introductcard" v-html="productinfo.content"></div>
+          </el-tab-pane>
+          <el-tab-pane label="体验方式" name="second">
+            <div class="introductcard" v-html="productinfo.expcontent"></div>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -39,33 +92,65 @@ export default {
   props: {
     appinfo: {
       type: Object,
-      default: null,
-    },
+      default: null
+    }
   },
   data() {
     return {
       Res: {},
       Inst: {},
       Member: null,
-      step: 2,
+      step: 3,
       initprivacy: "",
-      checked:false
+      checked: false,
+      sysproductlist: [],
+      productinfo: null,
+      showproductinfo: false,
+      activeName: "first",
+      selectproduct_id: 0,
+      pluginlist:[]
     };
   },
   created() {
     PageHelper.Init(this);
     PageHelper.LoginAuth(this);
-    HttpHelper.Post("content/get", { keycode: "initprivacy" }).then((content) => {
+    HttpHelper.Post("content/get", { keycode: "initprivacy" }).then(content => {
       this.initprivacy = Utils.HtmlDecode(content.content);
     });
+
+    HttpHelper.Post("store/productlist", { type: "S" }).then(sysproductlist => {
+      this.sysproductlist = sysproductlist;
+    });
+    HttpHelper.Post("store/pluginlist", {  }).then(pluginlist => {
+      this.pluginlist = pluginlist;
+    });
   },
-  methods: {},
+  methods: {
+    select(id) {
+      this.selectproduct_id = id;
+      this.showproductinfo = false;
+      this.step = 3;
+    },
+    viewdetail(id) {
+      this.productinfo = null;
+      this.activeName = "first";
+      HttpHelper.Post("store/productinfo", { id: id }).then(productinfo => {
+        productinfo.content = Utils.HtmlDecode(productinfo.content);
+        productinfo.expcontent = Utils.HtmlDecode(productinfo.expcontent);
+        this.productinfo = productinfo;
+        this.showproductinfo = true;
+      });
+    }
+  }
 };
 </script>
 <style scoped>
-
 .step1content {
   height: 300px;
   overflow-y: scroll;
+}
+.introductcard {
+  height: 300px;
+  overflow-y: auto;
 }
 </style>
